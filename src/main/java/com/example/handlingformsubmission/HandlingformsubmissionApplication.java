dbbeb43d953e55598c212b949cfad390b2ea7922
Tim <81175852+wharvex@users.noreply.github.com>
2025-02-07 18:15:21 -0500

    first commit

---
+++ src/main/java/com/example/handlingformsubmission/HandlingformsubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.handlingformsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingformsubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlingformsubmissionApplication.class, args);
+	}
+
+}
