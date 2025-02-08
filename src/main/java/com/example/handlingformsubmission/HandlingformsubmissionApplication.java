fdc645d19622c5c276976eeeb3657b5e40aa1168
Tim <81175852+wharvex@users.noreply.github.com>
2025-02-07 19:36:47 -0500

    complete handling form submission tutorial

--- src/main/java/com/example/handlingformsubmission/HandlingformsubmissionApplication.java
+++ src/main/java/com/example/handlingformsubmission/HandlingformsubmissionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HandlingformsubmissionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HandlingformsubmissionApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(HandlingformsubmissionApplication.class, args);
+    }
 
 }
